646c3c4d3a4691f809b71d7e3ebb49662a2234ad
Lucas01PL <lucas01_ce@hotmail.com>
2019-08-17 11:12:34 -0300

    Criação do Projeto

---
+++ src/main/java/br/com/sistemas/controleusuarioapi/ControleUsuarioApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.sistemas.controleusuarioapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleUsuarioApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleUsuarioApiApplication.class, args);
+	}
+
+}
