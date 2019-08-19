5ea75a6d15a0e63aea02ccc3501e5e3bb7616948
Lucas01PL <lucas01_ce@hotmail.com>
2019-08-19 08:09:17 -0300

    Melhorias na classes UsuarioController.

--- src/main/java/br/com/sistemas/controleusuarioapi/controller/UsuarioController.java
+++ src/main/java/br/com/sistemas/controleusuarioapi/controller/UsuarioController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.sistemas.controleusuarioapi.model.Usuario;
+import br.com.sistemas.controleusuarioapi.service.FuncaoService;
 import br.com.sistemas.controleusuarioapi.service.UsuarioService;
 
 @RestController
@@ -23,66 +25,99 @@ public class UsuarioController {
 	@Autowired
 	UsuarioService usuarioService;
 	
+	@Autowired
+	FuncaoService funcaoService;
+	
 	@RequestMapping(value="/", method=RequestMethod.GET)
 	public ResponseEntity<List<Usuario>> listarTodos() throws Exception {
-		return ResponseEntity.ok(usuarioService.listarTodos());
+		try {
+			List<Usuario> usuarios = usuarioService.listarTodos();
+			return ResponseEntity.ok(usuarios);
+		} catch(Exception e) {
+			e.printStackTrace();
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();	
+		}
 	}
 	
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
-	public ResponseEntity<Usuario> consultarPorId(@PathVariable(name="id") Long id) throws Exception {
+	public ResponseEntity<Usuario> consultarPorId(@PathVariable(name="id") String id) throws Exception {
 
-		if(id == null || id == 0) {
-			return ResponseEntity.notFound().build();
+		try {
+			if(id == null || id.isEmpty()) 
+				return ResponseEntity.badRequest().build();
+			
+			Usuario usuario = usuarioService.consultarPorId(new Long(id));
+			if(!Optional.of(usuario).isPresent()) 
+				return ResponseEntity.notFound().build();
+			
+			return ResponseEntity.ok(usuario);	
+		} catch(Exception e) {
+			e.printStackTrace();
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();	
 		}
-		
-		Usuario usuario = usuarioService.consultarPorId(id);
-		return ResponseEntity.ok(usuario);
 	}
 	
 	@RequestMapping(value="/cadastrar", method=RequestMethod.POST)
 	public ResponseEntity<Usuario> cadastrar(@RequestBody Usuario u) throws Exception {
 
-		if(!Optional.of(u).isPresent()) {
-			return ResponseEntity.badRequest().build();
+		try {
+			if(!Optional.of(u).isPresent()) {
+				return ResponseEntity.badRequest().build();
+			}
+			
+			if(u.getId() != null && u.getId() > 0) {
+				return ResponseEntity.badRequest().build();
+			}
+			
+			Usuario usuario = usuarioService.cadastrar(u);
+			usuario.setFuncao(funcaoService.consultarPorId(usuario.getFuncao().getId()));
+			return ResponseEntity.ok(usuario);
+		} catch(Exception e) {
+			e.printStackTrace();
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();	
 		}
-		
-		if(u.getId() != null && u.getId() > 0) {
-			return ResponseEntity.badRequest().build();
-		}
-		
-		Usuario usuario = usuarioService.cadastrar(u);
-		return ResponseEntity.ok(usuario);
 	}
 	
 	@RequestMapping(value="/alterar", method=RequestMethod.PUT)
 	public ResponseEntity<Usuario> alterar(@RequestBody Usuario u) throws Exception {
 
-		if(!Optional.of(u).isPresent()) {
-			return ResponseEntity.badRequest().build();
+		try {
+			if(!Optional.of(u).isPresent()) {
+				return ResponseEntity.badRequest().build();
+			}
+			
+			if(u.getId() == null || u.getId() <= 0) {
+				return ResponseEntity.badRequest().build();
+			}
+			
+			Usuario usuario = usuarioService.alterar(u);
+			usuario.setFuncao(funcaoService.consultarPorId(usuario.getFuncao().getId()));
+			return ResponseEntity.ok(usuario);
+		} catch(Exception e) {
+			e.printStackTrace();
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();	
 		}
-		
-		if(u.getId() == null || u.getId() <= 0) {
-			return ResponseEntity.badRequest().build();
-		}
-		
-		Usuario usuario = usuarioService.alterar(u);
-		return ResponseEntity.ok(usuario);
 	}
 	
 	@RequestMapping(value="/excluir/{id}", method=RequestMethod.DELETE)
 	public ResponseEntity<Usuario> excluir(@PathVariable(name="id") Long id) throws Exception {
 
-		if(id == null || id == 0) {
-			return ResponseEntity.notFound().build();
+		try {
+			if(id == null || id == 0) {
+				return ResponseEntity.notFound().build();
+			}
+			
+			Usuario usuario = usuarioService.consultarPorId(id);
+			if(!Optional.of(usuario).isPresent()) {
+				return ResponseEntity.notFound().build();
+			}
+	
+			usuarioService.excluir(usuario);
+			return ResponseEntity.ok(null);
+		} catch(Exception e) {
+			e.printStackTrace();
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();	
 		}
-		
-		Usuario usuario = usuarioService.consultarPorId(id);
-		if(!Optional.of(usuario).isPresent()) {
-			return ResponseEntity.notFound().build();
-		}
-
-		usuarioService.excluir(usuario);
-		return ResponseEntity.ok(null);
 	}
 
 }
