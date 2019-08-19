7ab8c9a8cba51a746a218a1b8b08423588436641
Lucas01PL <lucas01_ce@hotmail.com>
2019-08-19 08:05:16 -0300

    Implementação da classe ModelException.

---
+++ src/main/java/br/com/sistemas/controleusuarioapi/exception/ModelException.java
@@ -0,0 +1,11 @@
+package br.com.sistemas.controleusuarioapi.exception;
+
+public class ModelException extends Exception {
+
+	private static final long serialVersionUID = -6000785558640243480L;
+
+	public ModelException(String message) {
+		super(message);
+	}
+
+}
