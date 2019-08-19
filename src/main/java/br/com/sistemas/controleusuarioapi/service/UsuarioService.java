3a9e39eae0d6cf1e065323732ce73cc381446caf
Lucas01PL <lucas01_ce@hotmail.com>
2019-08-19 08:08:15 -0300

    Implementação de método responsável por validação na inclusão e alteração de usuário.

--- src/main/java/br/com/sistemas/controleusuarioapi/service/UsuarioService.java
+++ src/main/java/br/com/sistemas/controleusuarioapi/service/UsuarioService.java
@@ -3,12 +3,12 @@ package br.com.sistemas.controleusuarioapi.service;
 import java.util.List;
 import java.util.Optional;
 
-import javax.xml.bind.ValidationException;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import org.springframework.util.StringUtils;
 import br.com.sistemas.controleusuarioapi.dao.UsuarioDAO;
+import br.com.sistemas.controleusuarioapi.exception.ModelException;
 import br.com.sistemas.controleusuarioapi.model.Usuario;
 
 @Service
@@ -20,7 +20,7 @@ public class UsuarioService implements GenericoService<Usuario> {
 	@Override
 	public Usuario consultarPorId(Long id) throws Exception {
 		
-		if(id == null) throw new ValidationException("O parametro id é requerido!");
+		if(id == null) throw new ModelException("O parametro id é requerido!");
 		return usuarioDao.findById(id).orElse(null);
 	}
 
@@ -33,16 +33,16 @@ public class UsuarioService implements GenericoService<Usuario> {
 	@Override
 	public Usuario cadastrar(Usuario u) throws Exception {
 		
-		if(!Optional.of(u).isPresent()) throw new ValidationException("O parametro usuario é requerido!");
+		validarCamposCadastroOuAlteracao(u);
 		return usuarioDao.save(u);
 	}
 
 	@Override
 	public Usuario alterar(Usuario u) throws Exception {
 
-		if(!Optional.of(u).isPresent()) throw new ValidationException("O parametro usuario é requerido!");
+		validarCamposCadastroOuAlteracao(u);
 
-		if(u.getId() == null) throw new ValidationException("O parametro usuario não possuí ID válido!");
+		if(u.getId() == null) throw new ModelException("O parametro usuario não possuí ID válido!");
 		
 		return usuarioDao.save(u);
 	}
@@ -50,13 +50,27 @@ public class UsuarioService implements GenericoService<Usuario> {
 	@Override
 	public void excluir(Usuario u) throws Exception {
 
-		if(!Optional.of(u).isPresent()) throw new ValidationException("O parametro usuario é requerido!");
+		if(!Optional.of(u).isPresent()) throw new ModelException("O parametro usuario é requerido!");
 
-		if(u.getId() == null) throw new ValidationException("O parametro usuario não possuí ID válido!");
+		if(u.getId() == null) throw new ModelException("O parametro usuario não possuí ID válido!");
 		
 		usuarioDao.delete(u);
 		
 	}
 	
+	public void validarCamposCadastroOuAlteracao(Usuario usuario) throws Exception {
+		
+		if(!Optional.of(usuario).isPresent()) throw new ModelException("O parametro usuario é requerido!");
+		
+		if(StringUtils.isEmpty(usuario.getNome())) throw new ModelException("O parametro nome do usuario é requerido!");
+		
+		if(StringUtils.isEmpty(usuario.getCpf())) throw new ModelException("O parametro CPF do usuario é requerido!");
+		
+		if(!Optional.of(usuario.getFuncao()).isPresent()) throw new ModelException("O parametro função do usuario é requerido!");
+		
+		if(usuario.getFuncao().getId() == null || usuario.getFuncao().getId() == 0) throw new ModelException("O parametro função do usuario é requerido!");
+		
+		if(usuario.getSexo() == null) throw new ModelException("O parametro sexo do usuario é requerido!");
+	}
 
 }
